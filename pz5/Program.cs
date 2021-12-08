ae754b3883f44fc87ec2800423f7aa71941aeff5
Загуменников Ярослав <yzagumennikov1@mail.ru>
2021-12-08 21:09:37 +0500

    Edit

--- pz5/Program.cs
+++ pz5/Program.cs
@@ -6,7 +6,7 @@ namespace pz5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!fgdgdgdfgd");
         }
     }
 }
