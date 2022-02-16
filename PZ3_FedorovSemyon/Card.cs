c288aa9c91610869a1a28ca7e0e5c0b42533226d
Ярослав Загуменников <91648112+Yaroslav12062004@users.noreply.github.com>
2022-02-16 21:58:10 +0500

    Практика_ПЗ2_Семён

---
+++ PZ3_FedorovSemyon/Card.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PZ3_FedorovSemyon
+{
+    class Card
+    {
+        public int _number;
+        public string _clientFIO;
+        public int _validity;
+        public int _secureCode;
+        public string _type;
+        public Card(int number, string clientFIO, int validity, int secureCode, string type)
+        {
+            _number = number;
+
+            _clientFIO = clientFIO;
+
+            _validity = validity;
+
+            _secureCode = secureCode;
+
+            _type = type;
+
+        }
+        public Card()
+        {
+            _number = 0;
+
+            _clientFIO = "Отсутствует";
+
+            _validity = 0;
+
+            _secureCode = 0;
+
+            _type = "Отсутствует";
+
+        }
+        public Card(int number, int validity, int secureCode)
+        {
+            _number = number;
+
+            _clientFIO = "Данные отсутствуют";
+
+            _validity = validity;
+
+            _secureCode = secureCode;
+
+            _type = "Данные отсутствуют";
+
+        }
+        public Card(string clientFIO, string type)
+        {
+            _number = 0;
+
+            _clientFIO = clientFIO;
+
+            _validity = 0;
+
+            _secureCode = 0;
+
+            _type = type;
+
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($" Номер карты: {_number}\n Фио клиента: {_clientFIO}\n Срок действия: {_validity}\n Код безопасности: {_secureCode}\n Дебетовая/кредитная:{_type}");
+        }
+    }
+}
---
+++ PZ3_FedorovSemyon/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace PZ3_FedorovSemyon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Номер карты");
+            string Number = Console.ReadLine();
+            int NumbeR = 0;
+            if (Number == "")
+            {
+                NumbeR = 0;
+            }
+            else
+            {
+                NumbeR = Convert.ToInt32(Number);
+            }
+
+            Console.WriteLine("Срок действия");
+            string Validity = Console.ReadLine();
+            int ValiditY = 0;
+            if (Validity == "")
+            {
+                ValiditY = 0;
+            }
+            else
+            {
+                ValiditY = Convert.ToInt32(Validity);
+            }
+            Console.WriteLine("Код безопасности");
+            string code = Console.ReadLine();
+            int Code = 0;
+            if (code == "")
+            {
+                Code = 0;
+            }
+            else
+            {
+                Code = Convert.ToInt32(code);
+            }
+            Console.WriteLine("ФИО клиента");
+            string FIO = Console.ReadLine();
+            Console.WriteLine("Тип карты: Дебетовая/кредитная");
+            string type = Console.ReadLine();
+            Card card1 = new Card();
+            card1.Print();
+            Console.WriteLine("");
+            Card card2 = new Card(NumbeR, FIO, ValiditY, Code, type);
+            card2.Print();
+            Console.WriteLine("");
+            Card card3 = new Card(NumbeR, ValiditY, Code);
+            card3.Print();
+            Console.WriteLine("");
+            Card card4 = new Card(FIO, type);
+            card4.Print();
+            Console.WriteLine("");
+        }
+    }
+}
