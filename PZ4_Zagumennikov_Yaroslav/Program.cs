eebce92a8b40c3cecf83cb65d6380d0e3ca85bca
Ярослав Загуменников <91648112+Yaroslav12062004@users.noreply.github.com>
2022-02-18 18:01:20 +0500

    Готовое 4 практическое задание

--- PZ4_Zagumennikov_Yaroslav/Program.cs
+++ PZ4_Zagumennikov_Yaroslav/Program.cs
@@ -6,35 +6,9 @@ namespace PZ4_Zagumennikov_Yaroslav
     {
         static void Main(string[] args)
         {
-            DateTime ourTime= DateTime.Now;
-            string OurTime = Convert.ToString(ourTime);
-            Console.WriteLine("Имя\n");
-            string Name = Console.ReadLine();
-            Console.WriteLine("Фамилия\n");
-            string Fam = Console.ReadLine();
-            Console.WriteLine("Отчество\n");
-            string Otch = Console.ReadLine();
-            Console.WriteLine("Диагноз\n");
-            string Diagnos = Console.ReadLine();
-            Console.WriteLine("Дата рождения");
-            string a = Console.ReadLine();
-            DateTime dateBirthday = DateTime.Parse(a == "" ? "01.01.1990": a) ;
-            Console.WriteLine("Дата поступления");
-            string datePostuplenya = Console.ReadLine();
-            DateTime receiptData = DateTime.Parse(datePostuplenya == "" ? OurTime : datePostuplenya);
             
-            int ag = DateTime.Now.Year - dateBirthday.Year;
-            Patient patient = new Patient() ;
-
-            patient.Age = ag;
-            Console.WriteLine(patient.Age);
-
-            patient.DatePostup = receiptData;
-            Console.WriteLine(patient.DatePostup);
-
-            Patient patient1 = new Patient() ;
-            patient1.Print();
         }
     }
+
 }
 
