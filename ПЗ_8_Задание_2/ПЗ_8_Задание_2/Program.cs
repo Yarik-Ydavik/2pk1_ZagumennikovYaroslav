ed6ec52013e76f55a57b31eb2d862b4cd1474594
Ярослав Загуменников <91648112+Yaroslav12062004@users.noreply.github.com>
2022-04-29 23:29:41 +0500

    Add files via upload

---
+++ ПЗ_8_Задание_2/ПЗ_8_Задание_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ПЗ_8_Задание_2
+{
+    class Program
+    {
+        delegate void myDelegate(char A);
+
+        class myClass
+        {
+            public char i;
+            public void indexFirst(char b)
+            {
+                i = b;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var cell = new myClass[5];
+            for (int h = 0; h < cell.Length; h++)
+            {
+                cell[h] = new myClass();
+                myDelegate twelw = new myDelegate(cell[h].indexFirst);
+                twelw('u');
+            }
+
+
+            for (int h = 0; h < cell.Length; h++)
+            {
+                Console.WriteLine(cell[h].i);
+            }
+        }
+    }
+}
