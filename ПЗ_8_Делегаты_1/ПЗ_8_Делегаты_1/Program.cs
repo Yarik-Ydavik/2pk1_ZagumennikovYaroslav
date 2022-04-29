27c87db90d40e39681df275e0d3937c4ceed6de4
Ярослав Загуменников <91648112+Yaroslav12062004@users.noreply.github.com>
2022-04-29 23:28:55 +0500

    Add files via upload

---
+++ ПЗ_8_Делегаты_1/ПЗ_8_Делегаты_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ПЗ_8_Делегаты_1
+{
+    class Program
+    {
+        delegate int myDelegate(char A, string B);
+        static int countSymbols(char b, string a)
+        {
+            int g = 0;
+            char[] mas = a.ToCharArray();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] == b) g++;
+            }
+            return g;
+        }
+        static int indexFirst(char b, string a)
+        {
+            return a.IndexOf(b);
+        }
+        static void Main(string[] args)
+        {
+            myDelegate first = new myDelegate(countSymbols);
+            Console.WriteLine(first('l', "Purpple"));
+            myDelegate twelw = new myDelegate(indexFirst);
+            Console.WriteLine(twelw('l', "Purpple"));
+        }
+    }
+}
